c909c3d432a2c535861b238ba22101b4dd5c6f2e
nandkumar <nandkumargangai1@outlook.com>
2018-10-16 21:47:04 +0530

    Fetching images

--- js/index.js
+++ js/index.js
@@ -3,22 +3,25 @@ $(document).ready(function(){
     //document.getElementById("brands") = "";
     $("#mobiles").click(function(){
         //alert("mobiles");
-        //$("#res").append("Mobiles.");
+        document.getElementById("brands").innerHTML = "";
         document.getElementById("res").innerHTML = "Mobiles";
 
     });
 
     $("#laptops").click(function(){
         //alert("laptops");
-        document.getElementById("res").innerHTML = "laptops";
+        
         document.getElementById("brands").innerHTML = "";
         $("#brands").append('DELL: <input type="checkbox" name="brand" id="dell">');
         $("#brands").append('<br>HP: <input type="checkbox" name="brand" id="hp">');
         $("#brands").append('<br>ASUS: <input type="checkbox" name="brand" id="asus">');
         $.get("../JSON/laptops.json", function(data, status){            
             for(d of data){
-                $("#res").append(d.name);
-                $("#res").append("<br>");                
+                $("#res").append(`<div class="col-md-4 product float-left">
+                <p>${d.name}</p>
+                <img id="prodImg" src="./images/laptops/${d.image}">
+                <br>
+            `);                              
             }            
         });
 
@@ -29,6 +32,7 @@ $(document).ready(function(){
     $("#tv").click(function(){
         //alert("tv");
         document.getElementById("res").innerHTML = "tv";
+        document.getElementById("brands").innerHTML = "";
         $.get("../JSON/laptops.json", function(data, status){
             for(d of data){
                 $("#res").append(d.name);
@@ -39,6 +43,7 @@ $(document).ready(function(){
 
     $("#accessories").click(function(){
         //alert("accessories");
+        document.getElementById("brands").innerHTML = "";
         document.getElementById("res").innerHTML = "accessories";
     });
 });
