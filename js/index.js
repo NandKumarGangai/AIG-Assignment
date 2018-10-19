5802de02cccd2ed409bf81ba0e4dd9494d6a68a5
nandkumar <nandkumargangai1@outlook.com>
2018-10-19 12:55:48 +0530

    Added price ti laptop

--- js/index.js
+++ js/index.js
@@ -2,8 +2,10 @@ $(document).ready(function(){
     let dataToShow;
     //document.getElementById("brands") = "";
     $(".filters").hide();
+    $(".loader").hide();
     $("#mobiles").click(function(){
         //alert("mobiles");
+        $(".loader").show();
         $(".filters").show();
         document.getElementById("res").innerHTML = "";
         document.getElementById("brands").innerHTML = "";
@@ -21,12 +23,13 @@ $(document).ready(function(){
                 <p class="productName">${d.name}</p>
                 <img class="img-responsive img-thumbnail productImg" id="prodImgMob" src="./images/mobiles/${d.image}">
                 <br>
-                <div class="text-center">
+                <div class="text-center btns">
                 <button class="btn glyphicon glyphicon-heart text-danger"></button>
                 <button class="btn btn-success" type="button" onClick="checkStatus(${d.id})">Add To Cart</button>
                 </div>
             `);                              
-            }            
+            }
+            $(".loader").hide();
         });
 
     });
@@ -34,6 +37,7 @@ $(document).ready(function(){
     $("#laptops").click(function(){
         //alert("laptops");
         $(".filters").show();
+        $(".loader").show();
         document.getElementById("brands").innerHTML = "";
         document.getElementById("res").innerHTML = "";
         
@@ -51,12 +55,44 @@ $(document).ready(function(){
                 <p class="productName">${d.name}</p>
                 <img class="img-responsive img-thumbnail productImg" id="prodImgLaptop" src="./images/laptops/${d.image}">
                 <br>
-                <div class="text-center">
-                <button class="btn glyphicon glyphicon-heart"></button>
-                <button class="btn btn-success" type="button" onClick="checkStatus(${d.id})">Add To Cart</button>
+                <div class="desc">
+                <button class="btn btn-link" data-toggle="modal" data-target="#productModel">Know more...</button>
+                <div>
+                    <h4>&#8377; ${d.price}</h4>
+                </div>
+                <div id="productModel" class="modal fade" role="dialog">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">&times;</button>
+                        <h4 class="modal-title">${d.name}</h4>
+                    </div>
+                    <div class="modal-body">
+                        <p><img class="img-responsive img-thumbnail productImg" id="prodImgLaptop" src="./images/laptops/${d.image}"></p>
+                        <div class="">
+                            <p>${d.description}</p>
+                        </div>
+                        <div class="">
+                            <p><b>Price: </b>&#8377; ${d.price}</p>
+                        </div>
+                    </div>
+                    
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                    </div>
+
+                </div>
+                </div>
+
+                </div>
+                <div class="text-center btns">
+                    <button class="btn glyphicon glyphicon-heart"></button>
+                    <button class="btn btn-success" type="button" onClick="checkStatus(${d.id})">Add To Cart</button>
                 </div>
             `);                              
-            }            
+            }
+            $(".loader").hide();
         });
 
         var lfckv = document.getElementById("dell").checked;
