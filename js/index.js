889ce1050d981cab50100159d2f2a4d226f2735c
nandkumar <nandkumargangai1@outlook.com>
2018-10-22 12:55:58 +0530

    minor changes

--- js/index.js
+++ js/index.js
@@ -1,37 +1,20 @@
+var cartElements = [];
+var elements = ["hello"];
+
 $(document).ready(function(){
     let dataToShow;
     //document.getElementById("brands") = "";
     $(".filters").hide();
     $(".loader").hide();
+    showData("brands", "res", "mobile");
+
     $("#mobiles").click(function(){
         //alert("mobiles");
         $(".loader").show();
         $(".filters").show();
         document.getElementById("res").innerHTML = "";
         document.getElementById("brands").innerHTML = "";
-        $('#brands').append(`
-            <table>
-                <tr><td>Mi: </td><td>&nbsp;<input type="checkbox" name="brand" id="mi"></td></tr>
-                <tr><td>SAMSUNG: </td><td>&nbsp;<input type="checkbox" name="brand" id="samsung"></td></tr>
-                <tr><td>NOKIA: </td><td>&nbsp;<input type="checkbox" name="brand" id="nokia"></td></tr>
-            </table>
-        `);
-        
-        $.get("../JSON/mobiles.json", function(data, status){            
-            for(d of data){
-                $("#res").append(`<div class="col-md-4 col product well text-center">
-                <p class="productName">${d.name}</p>
-                <img class="img-responsive img-thumbnail productImg" id="prodImgMob" src="./images/mobiles/${d.image}">
-                <br>
-                <div class="text-center btns">
-                <button class="btn glyphicon glyphicon-heart text-danger"></button>
-                <button class="btn btn-success" type="button" onClick="checkStatus(${d.id})">Add To Cart</button>
-                </div>
-            `);                              
-            }
-            $(".loader").hide();
-        });
-
+        showData("brands", "res", "mobile");
     });
 
     $("#laptops").click(function(){
@@ -40,60 +23,8 @@ $(document).ready(function(){
         $(".loader").show();
         document.getElementById("brands").innerHTML = "";
         document.getElementById("res").innerHTML = "";
+        showData("brands", "res", "laptop");
         
-        $('#brands').append(`
-            <table>
-                <tr><td>DELL: </td><td>&nbsp;<input type="checkbox" name="brand" id="mi"></td></tr>
-                <tr><td>HP: </td><td>&nbsp;<input type="checkbox" name="brand" id="samsung"></td></tr>
-                <tr><td>ASUS: </td><td>&nbsp;<input type="checkbox" name="brand" id="nokia"></td></tr>
-            </table>
-        `);
-
-        $.get("../JSON/laptops.json", function(data, status){            
-            for(d of data){
-                $("#res").append(`<div class="col-md-4 product float-left well text-center">
-                <p class="productName">${d.name}</p>
-                <img class="img-responsive img-thumbnail productImg" id="prodImgLaptop" src="./images/laptops/${d.image}">
-                <br>
-                <div class="desc">
-                <button class="btn btn-link" data-toggle="modal" data-target="#productModel">Know more...</button>
-                <div>
-                    <h4>&#8377; ${d.price}</h4>
-                </div>
-                <div id="productModel" class="modal fade" role="dialog">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal">&times;</button>
-                        <h4 class="modal-title">${d.name}</h4>
-                    </div>
-                    <div class="modal-body">
-                        <p><img class="img-responsive img-thumbnail productImg" id="prodImgLaptop" src="./images/laptops/${d.image}"></p>
-                        <div class="">
-                            <p>${d.description}</p>
-                        </div>
-                        <div class="">
-                            <p><b>Price: </b>&#8377; ${d.price}</p>
-                        </div>
-                    </div>
-                    
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                    </div>
-                    </div>
-
-                </div>
-                </div>
-
-                </div>
-                <div class="text-center btns">
-                    <button class="btn glyphicon glyphicon-heart"></button>
-                    <button class="btn btn-success" type="button" onClick="checkStatus(${d.id})">Add To Cart</button>
-                </div>
-            `);                              
-            }
-            $(".loader").hide();
-        });
 
         var lfckv = document.getElementById("dell").checked;
         //alert(lfckv);
@@ -127,7 +58,104 @@ $(document).ready(function(){
         });
     });
     
+    $("").click(function(){
+
+    });
+    
 });
-function checkStatus(id){
-    alert(id);
+
+
+function showData(brands, res, category){
+    elements = [];
+    
+
+    $.get("../JSON/products.json", function(data, status){
+        var imgSize;
+        if(category == "mobile"){ imgSize= "prodImgMob";}else imgSize = "prodImgLaptop";
+        var brandsArray = [];
+        
+        for(d of data){
+            if(d.category == category && !brandsArray.includes(d.brand)){
+                brandsArray.push(d.brand);
+            }
+        }
+        console.log(brandsArray);
+        $('#'+brands).append(`<table>`);
+        for(brand of brandsArray){
+            $('#'+brands).append(`<tr><td>${brand}: </td><td>&nbsp;<input type="checkbox" name="brand" id=${brand}></td></tr>`);
+        }
+                
+        $('#'+brands).append(`</table>`);
+
+        for(d of data){
+            if(d.category == category){                
+                elements.push(d);
+                $("#"+res).append(`<div class="col-md-4 col-sm-6 col-xs-12 product float-left well text-center">
+                <p class="productName">${d.name}</p>
+                <img class="img-responsive img-thumbnail productImg" id=${imgSize} src="./images/${d.image}">
+                <br>
+                <div class="desc">
+                    <button class="btn btn-link" data-toggle="modal" data-target="#productModel" onClick="showModal('${d.name}', '${d.image}', '${d.description}', '${d.price}', '${imgSize}')">Know more...</button>
+                    <div>
+                        <h4>&#8377; ${d.price}</h4>
+                    </div>
+                        
+                </div>
+                <div class="text-center btns">
+                    <button class="btn glyphicon glyphicon-heart" onClick="addToWishList(${d.id})"></button>
+                    <button class="btn btn-success" type="button" onClick="addToCart(${d.id})">Add To Cart</button>
+                </div>
+            `); 
+            }                              
+        }
+        $(".loader").hide();
+        console.log(elements);
+    });    
+}
+
+function addToCart(id){
+    
+    var temp = {};
+    for(ele of elements){
+        if(ele.id == id){
+            temp = ele;
+            break;
+        }
+    }    
+    if(!cartElements.includes(temp)){
+        cartElements.push(temp);
+        alert("Product Added to Cart...");
+    }else{
+        alert("Product Already in Cart...");
+    }
+    console.log(cartElements);
+}
+
+function showModal(name, image, description, price, imgSize){
+    // alert(res);
+    //console.log(data);
+    $("#res").append(`
+    <div id="productModel" class="modal fade" role="dialog">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title">${name}</h4>
+            </div>
+            <div class="modal-body">
+                <p><img class="img-responsive img-thumbnail productImg" id=${imgSize} src="./images/${image}"></p>
+                <div class="">
+                    <p>${description}</p>
+                </div>
+                <div class="">
+                    <p><b>Price: </b>&#8377; ${price}</p>
+                </div>
+            </div>                        
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal" autofocus>Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+    `);
 }
