737e14cc5504edaa9091ad9aab2c63328dbc35d9
nandkumar <nandkumargangai1@outlook.com>
2018-10-28 19:20:55 +0530

    Added admin page

---
+++ Node/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const cors=require('cors');
+var fs = require('fs');
+var parser = require("body-parser");
+app.use(parser.json());
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    console.log("Hello");
+    fs.readFile('../JSON/products.json', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        var fileData = JSON.parse(data);
+        console.log(fileData);
+        res.end(data);
+    });
+})
+
+
+app.listen('8081', () => console.log('Listening on port 8080'));
